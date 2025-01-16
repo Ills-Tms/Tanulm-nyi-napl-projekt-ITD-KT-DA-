76fc0fa99d82db7eb69b5ae6c0ee38b1eed28d1c
ITD <a@a.a>
2025-01-16 11:33:14 +0100

    ora hozzaadva

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const datumsor =document.getElementById('datum')
-
+const ido=document.getElementById('ido')
 
 const date=new Date();
 
@@ -7,4 +7,8 @@ let ev = date.getFullYear();
 let honap=date.getMonth()+1;
 let nap=date.getDate();
 
+let ora= date.getHours();
+let perc=date.getMinutes();
+
+ido.innerText=ora+":"+perc
 datumsor.innerText=ev+"."+honap+"."+nap
