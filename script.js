9c6fc5e06c0ddec2d8113b288d0f874d7cfa1799
ITD <a@a.a>
2025-01-16 11:57:26 +0100

    ora fixed

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ let ev = date.getFullYear();
 let honap=date.getMonth()+1;
 let nap=date.getDate();
 
-let ora= date.getHours();
-let perc=date.getMinutes();
+let ora;
+let perc;
+
+function frissit(){
+
+ ora= date.getHours();
+ perc=date.getMinutes();
+}
+
+frissit()
+
 
 ido.innerText=ora+":"+perc
 datumsor.innerText=ev+"."+honap+"."+nap
+
+setInterval(frissit(),1000)
