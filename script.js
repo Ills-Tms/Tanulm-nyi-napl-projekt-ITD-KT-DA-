4a01f2186f9487251c71043587276144ee610e54
KT <KT@kt.kt>
2025-01-29 10:20:25 +0100

    Naptárba lehet beleírni

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const datumsor = document.getElementById("datum");
 const ido = document.getElementById("ido");
+const eventTimeSpan = document.getElementById("eventTime");
+const inputForm = document.getElementById("inputForm");
+const eventInput = document.getElementById("eventInput");
+let events = {};
 
 function ÓraFrissites() {
   const now = new Date();
@@ -24,3 +28,56 @@ setInterval(ÓraFrissites, 1000);
 
 setInterval(frissit, 1000);
 frissit();
+
+document.querySelectorAll(".clickable").forEach((cell) => {
+  cell.addEventListener("click", function () {
+    const day = this.getAttribute("data-day");
+    const time = this.getAttribute("data-time");
+    eventTimeSpan.textContent = `${day}. nap - ${time}`;
+    inputForm.style.display = "block";
+    eventInput.value = "";
+    eventInput.focus();
+
+    inputForm.setAttribute("data-day", day);
+    inputForm.setAttribute("data-time", time);
+  });
+});
+
+function saveEvent() {
+  const day = inputForm.getAttribute("data-day");
+  const time = inputForm.getAttribute("data-time");
+  const event = eventInput.value;
+
+  if (!event) {
+    alert("Kérlek írd be a terveidet");
+    return;
+  }
+
+  if (!events[day]) {
+    events[day] = {};
+  }
+  events[day][time] = event;
+
+  updateCalendar();
+
+  inputForm.style.display = "none";
+}
+
+function updateCalendar() {
+  document.querySelectorAll(".clickable").forEach((cell) => {
+    const day = cell.getAttribute("data-day");
+    const time = cell.getAttribute("data-time");
+    const eventCell = cell.querySelector(".event");
+    if (eventCell) {
+      eventCell.remove();
+    }
+
+    if (events[day] && events[day][time]) {
+      const event = events[day][time];
+      const eventElement = document.createElement("div");
+      eventElement.classList.add("event");
+      eventElement.textContent = event;
+      cell.appendChild(eventElement);
+    }
+  });
+}
